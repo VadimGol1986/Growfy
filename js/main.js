ee2ca4e0e2c38e26f5a617413ccb1b549273f886
VadymGol1986 <104563255+VadimGol1986@users.noreply.github.com>
2023-02-18 19:51:36 +0200

    clean empty rules

--- js/main.js
+++ js/main.js
@@ -9,3 +9,40 @@ function documentClick(e) {
         document.documentElement.classList.toggle('menu-open');
     }
 }
+
+
+//Пропадає плейсхолдер при фокусі на форму з імям subscribeForm в полі з імям subscribeInput
+const subscribeForm = document.forms.subscribeForm;
+const subscribeFormInput = subscribeForm.subscribeInput;
+const subscribeFormInputPlaceholder = subscribeFormInput.placeholder;
+
+subscribeFormInput.addEventListener("focus", function (e) {
+    subscribeFormInput.placeholder = "";
+});
+subscribeFormInput.addEventListener("blur", function (e) {
+    subscribeFormInput.placeholder = subscribeFormInputPlaceholder;
+});
+
+// Перевіряєм чи коректний емейл
+subscribeForm.addEventListener("submit", function (event) {
+    if (emailTest(subscribeFormInput)) {
+        subscribeFormInput.parentElement.insertAdjacentHTML(
+            "afterbegin",
+            `<div style="color: red;" class="main-form__error">
+            enter valid email
+            </div>`
+        );
+        event.preventDefault();
+    }
+
+    subscribeFormInput.addEventListener("focus", function (event) {
+        if (subscribeFormInput.previousElementSibling) {
+            subscribeFormInput.previousElementSibling.remove();
+        }
+    });
+});
+
+// Функція тесту email
+function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+};
